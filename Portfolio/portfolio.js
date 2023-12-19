ea2e9e1eaeab94372f2028e72ee293d17bb31f15
Umaima Ali <140103366+Um9503@users.noreply.github.com>
2023-12-20 01:29:17 +0530

    Add files via upload

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,36 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+const personalImage = document.getElementById('personal-image');
+
+personalImage.addEventListener('mouseenter', () => {
+    personalImage.classList.add('rotate');
+});
+
+personalImage.addEventListener('mouseleave', () => {
+    personalImage.classList.remove('rotate');
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
